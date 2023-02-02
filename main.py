7145ed02c266cc77a4a357af8baf1bd659103142
rtivalencia <123529994+rtivalencia@users.noreply.github.com>
2023-02-02 17:25:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
+def gcd():
 
-def gcd(a, b):
-    if a == 0:
-        return b
-    if 0 < a < b:
-        return a
+    a = int(input("Enter the first value: "))       # 81, prompts for user input
+    b = int(input("Enter the second value:  "))     # 57
+    r = a % b          # calcs for remainder        # 24
+    while (r != 0):     # while loop meanwhile r does not = 0
+        a = b           # subs values for calculation of the algorithm
+        b = r
+        r = a % b       # new remainder and will loop as long as it does not = 0
+    print('GCD is:', b) # output, 3
 
-    return gcd(a % b, b)
+
 
 
 # Driver code
 if __name__ == "__main__":
-    a = int(input("Enter the first value here: "))
-    b = int(input("Enter the second value here: "))
-
-    print("gcd(", a, ",", b, ") = ", gcd(a, b))
-
-
-
+    print (gcd())
